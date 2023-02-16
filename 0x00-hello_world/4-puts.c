e5b610fbdc77d6004c319f467de197c0e6f564b8
theo_opare <theo@example.com>
2023-02-16 12:20:00 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
     * Return: return 0 if successful or 1 if failed
     */
 
-    int main(void)
-    {
-            puts("\"Programming is like building a multilingual puzzle");
-            return(0);
-    }
+   int main(void)
+   {
+           puts("\"Programming is like building a multilingual puzzle");
+           return(0);
+   }
