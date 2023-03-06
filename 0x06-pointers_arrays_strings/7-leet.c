5e8ad25b017d7d8b07da874c8c1f9933d0a31d4b
theo_opare <theo@example.com>
2023-03-05 19:18:19 -0800

    https://intranet.alxswe.com/projects/217#task-897:~:text=0x06.%20C%20%2D%20More%20pointers%2C%20arrays%20and%20strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value 
+ * Return: n vlaue 
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeoOtTIL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j];
+				
+					
